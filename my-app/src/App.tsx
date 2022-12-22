6ccc2096751922df7ef4d9b9e7993509794b8df6
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2022-12-21 22:30:54 -0600

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function DictApp() {
+  const [data, setData] = React.useState<DictAppObj[]>([]);
+  const [word, setWord] = React.useState<string>("default");
+
+  const newWord = (wordInput: string) => {
+    const result = (value: Response) => {
+      return value.json();
+    };
+    const jsonData = (value: DictAppObj[]) => {
+      setData(value);
+    };
+
+    return fetch(`https://api.api-ninjas.com/v1/dictionary?word=${wordInput}`, {
+      headers: { "X-Api-Key": "lZOkNRcxcRbTIitJ2BjLKw==7ukbxScp64NxhMke" },
+    })
+      .then(result)
+      .then(jsonData);
+  };
+
+  React.useEffect(() => {
+    newWord(word);
+  }, [word]);
+
   return (
     <div>
+      <h2>{data[0]?.word || "Loading"}</h2>
+      <form action="">
+        <input type="text" />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
-
-export default App;
