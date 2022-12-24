2b1a166551b330f5dcce991d5d6ddaa501017853
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2022-12-24 00:56:31 -0600

    Not really sure what this push is for

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import FormikForm from './Molecules/FormikForm';
+import FormikForm from './Organisms/FormikForm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
