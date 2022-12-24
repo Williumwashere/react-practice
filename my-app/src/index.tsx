4f67606096108729954e30dc31cbdca461dade59
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2022-12-23 23:31:42 -0600

    Folders Update

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Form from './Form';
-import FormikForm from './Organisms/FormikForm';
+import FormikForm from './Molecules/FormikForm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
