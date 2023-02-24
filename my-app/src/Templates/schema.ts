b72f9f9555e037b0d6e0a7d80f23dbf02bb76018
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2023-02-23 18:47:43 -0600

    up

--- my-app/src/Templates/schema.ts
+++ my-app/src/Templates/schema.ts
@@ -9,5 +9,5 @@ export const schema = Yup.object({
     .required("Input is required")
     .min(5, "Input must be at least 5 characters")
     .max(20, "Cannot exceed 20 characters."),
-    age: Yup.number().required(),
+  age: Yup.number().required(),
 });
